a36b8046cc62051a3da3f8abd72aa98dd2327388
Aweb Mekhilef <awebmekhilef@gmail.com>
2021-05-28 16:56:28 +0800

    Initial commit

---
+++ Terrarium/Main.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace Terrarium
+{
+	public class Main : Game
+	{
+		GraphicsDeviceManager _graphics;
+		SpriteBatch _spriteBatch;
+
+		public Main()
+		{
+			_graphics = new GraphicsDeviceManager(this);
+			Content.RootDirectory = "Content";
+			IsMouseVisible = true;
+		}
+
+		protected override void Initialize()
+		{
+			base.Initialize();
+
+			_graphics.PreferredBackBufferWidth = 1280;
+			_graphics.PreferredBackBufferHeight = 720;
+			_graphics.ApplyChanges();
+		}
+
+		protected override void LoadContent()
+		{
+			_spriteBatch = new SpriteBatch(GraphicsDevice);
+		}
+
+		protected override void Update(GameTime gameTime)
+		{
+			if (Keyboard.GetState().IsKeyDown(Keys.Escape))
+				Exit();
+		}
+
+		protected override void Draw(GameTime gameTime)
+		{
+			GraphicsDevice.Clear(Color.LightSkyBlue);
+		}
+	}
+}
